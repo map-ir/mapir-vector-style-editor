1d0f28b8307b37c58080ae8a94ca38a1141d87e2
Reyhane Masumi <r.masumi94@gmail.com>
2022-09-27 19:48:12 +0330

    prepare repo

---
+++ demo/src/App.tsx
@@ -0,0 +1,12 @@
+import MapirStyleEditor from 'mapir-vector-style-editor';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MapirStyleEditor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const MapirStyleEditor = () => {
+  return 'HI';
+};
+
+export default MapirStyleEditor;
